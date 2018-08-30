485faa0eaac083f0bd69a605eee5f27e4efdaadc
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-08-30 11:47:55 +0200

    tweaks and fix bug

--- R/use_ld_contributing.R
+++ R/use_ld_contributing.R
@@ -3,8 +3,15 @@
 #'
 #' @export
 use_ld_github <- function(package_name){
-  use_contributing_template(package_name = package_name)
-  usethis::use_build_ignore(".github")
+  suppressWarnings(
+    use_contributing_template())
+  usethis::use_template("CODE_OF_CONDUCT.md", 
+               save_as = "CODE_OF_CONDUCT.md")
+  usethis::use_build_ignore(c(".github",
+                              "CODE_OF_CONDUCT.md"))
+  message(glue::glue("Paste this at the bottom of the README.md:\n",
+"\n Wanna report a bug or suggest a feature? Great stuff! For more information on how to contribute check out [our contributing guide](.github/CONTRIBUTING.md). \n \n Please note that this R package is released with a [Contributor Code of Conduct](CODE_OF_CONDUCT.md). By participating in this package project you agree to abide by its terms.
+                     "))
 }
 
 # Use CONTRIBUTING template
