d80f3db38d9d3da7b97bf1c395fee28e510946bc
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-10-21 08:31:27 +0200

    quick fix for #11

--- R/use_ld_labels.R
+++ R/use_ld_labels.R
@@ -6,6 +6,8 @@
 #'
 use_ld_labels <- function(){
   
+  repo <- usethis:::github_repo_spec()
+  
   labels <- c("bug :bug:" = "EAEF49",
               "documentation :page_with_curl:" = "23FDF2",
               "duplicate :dancers:" = "D4A484",
@@ -14,6 +16,17 @@ use_ld_labels <- function(){
               "help wanted :raised_hand:" = "E8830C",
               "question :question:" = "798FE7",
               "wontfix :see_no_evil:" = "F0FA1E")
-  usethis::use_github_labels(labels,
+  
+  rename <- c("bug :bug:", "documentation :page_with_curl:", "duplicate :dancers:", 
+              "enhancement :sparkles:", "good first issue :hatching_chick:", 
+              "help wanted :raised_hand:", "question :question:", "wontfix :see_no_evil:"
+  )
+  names(rename) <- c("bug", "documentation", "duplicate", "enhancement", "good first issue", 
+                     "help wanted", "question", "wontfix")
+  
+  usethis::use_github_labels(repo_spec = repo,
+                             labels = names(labels),
+                             rename = rename,
+                             colours = labels,
                              delete_default = TRUE)
 }
